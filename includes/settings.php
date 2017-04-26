5ef49875e17c273d5f85ce1ff75f9b1631d9d2a8
rok6 <sechs06re.ryo+glbd@gmail.com>
2017-04-26 18:54:18 +0900

    first commit

--- includes/settings.php
+++ includes/settings.php
@@ -1,24 +1,82 @@
-<?php //update-nag
-require_once( ABSPATH . 'wp-admin/admin-header.php' );
-?>
+<?php
 
+?>
 <div class="wrap">
+
 	<h1>WPRD Folders Media</h1>
 
-	<ul><?php foreach( $files as $file ) : ?>
+	<form method="post" action="options.php">
+		<?php settings_fields($setting_group); ?>
 
-		<li id="media_<?=esc_attr($file->ID)?>">
-			<div class="post_title"><?=esc_html($file->post_title)?></div>
-			<div class="post_url"><?=esc_html($file->guid)?></div>
-			<div class="post_date"><?=esc_html($file->post_date)?></div>
-			<div class="post_modified"><?=esc_html($file->post_modified)?></div>
-		</li>
+		<?php do_settings_sections($page_name); ?>
 
-	<?php endforeach; ?></ul>
+		<?php submit_button(); ?>
+	</form>
+
 </div>
 
 <?php //<!-- wpbody-content -->
-//include( ABSPATH . 'wp-admin/admin-footer.php' );
+// global $wp_settings_sections, $wp_settings_fields;
+//
+// _d($wp_settings_sections);
+// _d($wp_settings_fields);
 
-//_d($files);
-_d($post_types);
+_d($params);
+
+// _d($post_types);
+?>
+<script id="wrfm-scripts">
+
+var WRFM = WRFM || {};
+
+WRFM.param = {
+	plugin_name	: <?=wrfm_js_encode($plugin_name)?>,
+	post_types	: <?=wrfm_js_encode($post_types)?>
+};
+
+(function(window, $) {
+
+	'use strict';
+
+	var $filters = $('#wrfm-filters');
+	var $new_button = $('#wrfm-new_filter');
+
+	$new_button.on('click', function() {
+		add_new_dirs();
+	});
+
+	function add_new_dirs() {
+		var props = '<li>';
+			props += build_select(WRFM.param.post_types);
+			props += '<span class="show_directory"></span>';
+			props += '<span class="remove">×</span>';
+			props += '</li>';
+
+		$filters.append(props);
+
+		$filters.children.off('change', 'select', function() {
+
+		})
+
+		$(props).on('click', 'select', function(e) {
+			console.log(this);
+			$(this).attr('name', WRFM.param.plugin_name +'[dirs][]');
+		});
+	}
+
+	function build_select($data) {
+
+		var html = '';
+
+		html = '<select name="'+ WRFM.param.plugin_name +'[dirs][]">';
+		$.each($data, function(k, v) {
+			html += '<option value="'+ k +'">'+ v +'</option>';
+		});
+		html += '</select>';
+
+		return html;
+	}
+
+})(this, this.jQuery);
+
+</script>
--- wprd-folders-media.php
+++ wprd-folders-media.php
@@ -54,29 +54,35 @@ class WPRD_FoldersMedia
 	private static $object;
 	private static $version = 0.1;
 
-	private static $prefix		= 'wrfm';
-	private static $plugin_hook	= 'wprd-folders-media';
 	private static $plugin_name = 'WPRD Folders Media';
+	private static $plugin_hook	= 'wprd-folders-media';
+	private static $prefix		= 'wrfm';
+	private static $admin_menu_hook;
 
+	private static $setting_page_name = 'wprd-folders-media';
+	private static $setting_group = 'wprd-folders-media-group';
+
+	private static $plugin_menu_role = 'administrator';
+	private static $upload_dir_base;
+	private static $upload_dir_url;
+
+	private $get_params;
 	private $vars = [];
-
-	private $upload_dir;
-	private $hook;
+	private $options = [];
 
 	public function __construct()
 	{
 		//
 	}
 
-
-	/**
-	 *
-	 */
+	/**=====================================================
+	 *	WRFM Setup
+	 *=====================================================*/
 	public function initialize()
 	{
 		load_plugin_textdomain(self::$plugin_hook, false, dirname( plugin_basename(__FILE__ ) ) . '/languages' );
 
-		define( 'WRFM_BASENAME', plugin_basename(__FILE__) ); // WPRD_Folders_Media/wprd-folders-media.php
+		define( 'WRFM_BASENAME', plugin_basename(__FILE__) ); // wprd-folders-media/wprd-folders-media.php
 		define( 'WRFM_UPLOAD_DIR', '' );
 
 		$path = wp_upload_dir();
@@ -84,9 +90,11 @@ class WPRD_FoldersMedia
 		$path['basedir'] = preg_replace('/\/[^\/]+\/[\.]{2}/u', '', $path['basedir']);
 		$path['basedir'] = str_replace('/', '\\', $path['basedir']);
 
-		$this->pre_upload_functions();
-		$this->upload_functions();
-		$this->xmlrpc_call();
+		self::$upload_dir_base = $path['basedir'];
+		self::$upload_dir_url = $path['baseurl'];
+
+		$this->pre_upload_functions();	// アップロード前の処理
+		$this->uploaded_functions();	// アップロード後の処理
 		$this->admin_menu();
 	}
 
@@ -96,28 +104,21 @@ class WPRD_FoldersMedia
 	 */
 	private function pre_upload_functions()
 	{
-		add_filter('wp_handle_upload_prefilter', function( $f ) {
-			echo 'wp_handle_upload_prefilter','::'; _d($f);
+		add_filter('wp_handle_upload_prefilter', function( $file ) {
+			add_filter('upload_dir', [&$this, 'wprd_upload_dir']);
+			return $file;
 		});
 	}
+
 
 	/**
 	 *
 	 */
-	private function upload_functions()
+	private function uploaded_functions()
 	{
-		add_filter('wp_handle_upload', function( $f ) {
-			echo 'wp_handle_upload','::'; _d($f);
-		});
-	}
-
-	/**
-	 *
-	 */
-	private function xmlrpc_call()
-	{
-		add_action('xmlrpc_call', function( $f ) {
-			echo 'xmlrpc_call','::'; _d($f);
+		add_filter('wp_handle_upload', function( $file ) {
+			remove_filter('upload_dir', [&$this, 'wprd_upload_dir']);
+			return $file;
 		});
 	}
 
@@ -136,15 +137,19 @@ class WPRD_FoldersMedia
 	{
 		add_action('admin_menu', function() {
 
-			$this->hook = add_submenu_page('upload.php',
+			self::$admin_menu_hook = add_submenu_page('upload.php',
 				self::$plugin_name,
 				self::$plugin_name,
-				'administrator',
-				self::$plugin_hook,
+				self::$plugin_menu_role,
+				self::$setting_page_name,
 				[&$this, 'admin_page']
 			);
 			$this->admin_scripts();
 		});
+
+		add_action('admin_init', [&$this, 'register_setting_fields']);
+
+		$this->get_params = get_option(self::$plugin_hook);
 	}
 
 	/**
@@ -152,8 +157,12 @@ class WPRD_FoldersMedia
 	 */
 	public function admin_page()
 	{
+		$this->vars['params'] = $this->get_params;
 		$this->vars['files'] = $this->get_files();
 		$this->vars['post_types'] = $this->get_post_types();
+		$this->vars['plugin_name'] = self::$plugin_hook;
+		$this->vars['setting_group'] = self::$setting_group;
+		$this->vars['page_name'] = self::$setting_page_name;
 
 		extract($this->vars);
 		include('includes/settings.php');
@@ -165,13 +174,18 @@ class WPRD_FoldersMedia
 	 */
 	public function admin_scripts()
 	{
-		add_action('admin_print_scripts-'.$this->hook, function() {
+		add_action('admin_print_scripts-'.self::$admin_menu_hook, function() {
 			wp_enqueue_media();
+			wp_enqueue_style(self::$prefix.'-styles',
+				plugins_url('/assets/css/style.css', __FILE__),
+				false,
+				filemtime( dirname(__FILE__) . '/assets/css/style.css' )
+			);
 			wp_enqueue_script(self::$prefix.'-scripts',
-				plugins_url('/assets/js/media-uploader.js', __FILE__),
+				plugins_url('/assets/js/settings_scripts.js', __FILE__),
 				['jquery'],
-				filemtime( dirname(__FILE__) . '/assets/js/media-uploader.js' ),
-				false
+				filemtime( dirname(__FILE__) . '/assets/js/settings_scripts.js' ),
+				true
 			);
 		});
 	}
@@ -180,6 +194,15 @@ class WPRD_FoldersMedia
 	/**=====================================================
 	 *	WRFM Methods
 	 *=====================================================*/
+	/**
+	 *
+	 */
+	private function wprd_upload_dir( $param )
+	{
+		$this->parara = $param;
+		return $param;
+	}
+
 	/**
 	 *
 	 */
@@ -214,7 +237,97 @@ class WPRD_FoldersMedia
 		return $types;
 	}
 
+	/**
+	 * @since 0.1
+	 */
+	private function add_settings_field( $id, $title, array $args = [] )
+	{
+		$_id = ($id !== 'uploads_use_yearmonth_folders') ? self::$plugin_hook : $id;
 
+		add_settings_field($_id, __($title),
+			[$this, 'build_setting_fields'],
+			self::$setting_page_name,
+			'default',
+			$args += ['id' => $id, 'title' => $title, 'label_for' => $id]
+		);
+	}
+
+	public function register_setting_fields() {
+
+		add_settings_section(
+			'default',
+			null,
+			null,
+			self::$setting_page_name
+		);
+
+		$this->add_settings_field( 'uploads_use_yearmonth_folders', 'フォルダを年月ベースにする' );
+		$this->add_settings_field( 'settings', '基本設定' );
+
+		register_setting(self::$setting_group, 'uploads_use_yearmonth_folders');
+		register_setting(self::$setting_group, self::$plugin_hook, [&$this, 'sanitize']);
+	}
+
+	public function build_setting_fields( $args )
+	{
+		$id = esc_attr($args['id']);
+
+		switch( $id )
+		{
+			case 'settings':
+				$this->options = get_option(self::$plugin_hook);
+
+				$value = $this->get_param('default_dir');
+
+				echo '<input name="'.self::$plugin_hook.'['.$id.']" type="text" id="'.$id.'" value="'. esc_attr($value) .'" class="regular-text" />',
+					 '<p class="description">',
+					 	'下記、投稿タイプごとの指定がない場合のフォルダ作成の基本設定です。<br />',
+					 	'空欄の場合はフォルダを作成しません。「/」で階層を分けることができ、空白があった場合は自動的に削除され詰められます。',
+					 '</p>';
+
+
+
+				echo '<div class="wrfm-container">',
+						'<ul id="wrfm-filters">';
+
+						if( !empty($this->options['dirs']) ) {
+
+		 					foreach( $this->options['dirs'] as $dir_set ) {
+		 						echo '<li>',
+										'<select name="'. self::$plugin_hook .'[dirs][]">',
+											'<option></option>',
+										'</select>',
+									 '</li>';
+		 					}
+
+		 				}
+
+				echo	'</ul>',
+						'<div id="wrfm-new_filter">',
+							'<p class="wrfm-new_filter-info">'. __('投稿タイプの毎のフォルダ条件を追加', self::$plugin_hook) .'</p>',
+						'</div>',
+					 '</div>';
+				break;
+
+			case 'uploads_use_yearmonth_folders':
+				echo '<input name="'.$id.'" type="hidden" value="0" />',
+			 		 '<label for="'.$id.'"><input name="'.$id.'" type="checkbox" id="'.$id.'" value="1" '. checked(1, get_option($id), false) .' /></label>';
+				break;
+		}
+	}
+
+	public function get_param($id)
+	{
+		return isset($this->options[$id]) ? (string) $this->options[$id] : '';
+	}
+
+	public function sanitize( $input )
+	{
+		_d($input);
+		add_settings_error( self::$setting_page_name, self::$plugin_hook, 'メッセージを入力して下さい。' );
+
+		return $input;
+	}
 
 
 	/**=====================================================
@@ -245,10 +358,9 @@ class WPRD_FoldersMedia
 
 	private function default_settings()
 	{
-		_d('default_settings');
-
 		$defaults = [
-			''	=> '',
+			'dirs' => [],
+			'default_dir' => '',
 		];
 
 		$before = get_option(self::$plugin_hook);
@@ -262,8 +374,6 @@ class WPRD_FoldersMedia
 
 	private function remove_settings()
 	{
-		_d('remove_settings');
-
 		delete_option(self::$plugin_hook);
 	}
 
@@ -279,8 +389,12 @@ class WPRD_FoldersMedia
  * @since 0.1
  * @param $str
  */
-function _d( $str ){
+function _d( $str ) {
 	echo PHP_EOL,'<pre>';
 	var_dump($str);
 	echo '</pre>',PHP_EOL;
+}
+
+function wrfm_js_encode($data) {
+	return json_encode($data, JSON_HEX_TAG | JSON_HEX_AMP | JSON_HEX_APOS | JSON_HEX_QUOT);
 }
